4113b349d209cc1274aee1a62667fd84fb60713f
realstevenlol <jackfdevelopment@gmail.com>
2023-01-30 12:14:36 +0000

    Renamed login pages and css to index equivalents.

--- assets/js/responsive.js
+++
@@ -1,7 +0,0 @@
-let vNav = document.querySelector('.vertical-nav');
-let hamburger = document.querySelector('.ri-menu-line');
-
-hamburger.onclick = function() {
-    console.log('clicked');
-    vNav.classList.toggle('active');
-}

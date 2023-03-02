959f9b8d99c046af8716a4c59ad04291a5e5267c
stevenlol <jackfdevelopment@gmail.com>
2023-03-02 19:56:00 +0000

    Very minor changes

--- amendview.php
+++ amendview.php
@@ -1,55 +1,54 @@
-    <?php
+<?php
 
-    include_once('includes/dbh.inc.php');
-    include_once('includes/addsupplier.functions.php');
+include_once('includes/dbh.inc.php');
 
-    if (!isset($_SESSION)) {
-        session_start();
-    }
-    ?>
+if (!isset($_SESSION)) {
+    session_start();
+}
+?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pharmacy</title>
-        <link rel="stylesheet" href="css/amendview.css">
-        <link rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
-            <script src="js/date.js" defer></script>
-            <script src="js/amendview.js" defer></script>
-            <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    </head>
-    <body>
-    <div class="horizonal-nav">
-        <span id="time"></span>
-        <div class="logo-container">
-            <i class="ri-capsule-line"></i>
-            <span id="logo-title"> | BP</span>
-        </div>
-        <div class="account-container">
-            <button>
-                <span class="accountId">Logout</span>
-            </button>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacy</title>
+    <link rel="stylesheet" href="css/amendview.css">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+    <script src="js/date.js" defer></script>
+    <script src="js/amendview.js" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+<body>
+<div class="horizonal-nav">
+    <span id="time"></span>
+    <div class="logo-container">
+        <i class="ri-capsule-line"></i>
+        <span id="logo-title"> | BP</span>
     </div>
-    <div class="main-container">
-        <div class="vertical-nav">
-            <a href="#">Drugs</a>
-            <a href="#">Stock Control</a>
-            <a href="#">Doctor</a>
-            <a href="#">Customer</a>
-            <a href="suppliers.php" class="selected">Supplier</a>
-        </div>
-        <main>
-            <form name="form" action="includes/amendview.inc.php" method="post" class="info-wrapper">
-                <div class="info">
-                    <label for="select">Supplier </label>
-                    <select name="select" id="select" onchange="change()">
-                    <?php 
-                    
+    <div class="account-container">
+        <button>
+            <span class="accountId">Logout</span>
+        </button>
+    </div>
+</div>
+<div class="main-container">
+    <div class="vertical-nav">
+        <a href="#">Drugs</a>
+        <a href="#">Stock Control</a>
+        <a href="#">Doctor</a>
+        <a href="#">Customer</a>
+        <a href="suppliers.php" class="selected">Supplier</a>
+    </div>
+    <main>
+        <form name="form" action="includes/amendview.inc.php" method="post" class="info-wrapper">
+            <div class="info">
+                <label for="select">Supplier </label>
+                <select name="select" id="select" onchange="change()">
+                    <?php
+
                     $stmt = $conn->prepare("SELECT * FROM Suppliers WHERE deleted = 0");
                     $stmt->execute();
                     $result = $stmt->get_result();
@@ -65,61 +64,62 @@
                         $infoString = htmlspecialchars($infoString);
                         echo "<option value=\"$infoString\">$name</option>";
                     }
-                    
+
                     ?>
-                    </select>
-                </div>
+                </select>
+            </div>
 
-                <div class="info">
-                    <label for="id">ID </label>
-                    <input type="text" name="id" id="id" disabled>
-                </div>
+            <div class="info">
+                <label for="id">ID </label>
+                <input type="text" name="id" id="id" disabled required>
+            </div>
 
-                <div class="info">
-                    <label for="name">Name </label>
-                    <input type="text" name="name" id="name" disabled>
-                </div>
+            <div class="info">
+                <label for="name">Name </label>
+                <input type="text" name="name" id="name" disabled required>
+            </div>
 
-                <div class="info">
-                    <label for="address">Address </label>
-                    <input type="text" name="address" id="address" disabled>
-                </div>
+            <div class="info">
+                <label for="address">Address </label>
+                <input type="text" name="address" id="address" disabled required>
+            </div>
 
-                <div class="info">
-                    <label for="email">Email </label>
-                    <input type="text" name="email" id="email" disabled>
-                </div>
+            <div class="info">
+                <label for="email">Email </label>
+                <input type="email" name="email" id="email" disabled required>
+            </div>
 
-                <div class="info">
-                    <label for="website">Website </label>
-                    <input type="text" name="website" id="website" disabled>
-                </div>
+            <div class="info">
+                <label for="website">Website </label>
+                <input type="url" name="website" id="website" disabled required>
+            </div>
 
-                <div class="info">
-                    <label for="telephone">Telephone </label>
-                    <input type="text" name="telephone" id="telephone" disabled>
-                </div>
+            <div class="info">
+                <label for="telephone">Telephone </label>
+                <input type="text" name="telephone" id="telephone" pattern="\d{9,}" disabled required>
+            </div>
 
-                <div class="buttons">
-                    <div class="amend-buttons">
-                        <button id="amend" type="button" onclick="toggle()">Amend</button>
-                        <button id="save" type="button" onclick="confirmSave()">Save Changes</button>
-                    </div>
+            <div class="buttons">
+                <div class="amend-buttons">
+                    <button id="amend" type="button" onclick="toggle()">Amend</button>
+                    <button id="save" type="button" onclick="confirmSave()">Save Changes</button>
+                    <button id="hidden" type="submit"></button>
                 </div>
+            </div>
 
-                <div class="err">
-                    <?php 
-                    if (isset($_GET['amend'])) {
-                        if ($_GET['amend'] == "success") {
-                            echo "<p class='success'>Amend successful</p>";
-                        } else if ($_GET['amend'] == "failed") {
-                            echo "<p class='fail'>Amend failed</p>";
-                        }
+            <div class="err">
+                <?php
+                if (isset($_GET['amend'])) {
+                    if ($_GET['amend'] == "success") {
+                        echo "<p class='success'>Amend successful</p>";
+                    } else if ($_GET['amend'] == "failed") {
+                        echo "<p class='fail'>Amend failed</p>";
                     }
-                    ?>
-                </div>
-            </form>
-        </main>
-    </div>
-    </body>
-    </html>
+                }
+                ?>
+            </div>
+        </form>
+    </main>
+</div>
+</body>
+</html>
--- js/amendview.js
+++ js/amendview.js
@@ -1,58 +1,60 @@
-    const select = document.getElementById('select');
-    const amendButton = document.getElementById('amend');
-    const saveButton = document.getElementById('save');
+const select = document.getElementById('select');
+const amendButton = document.getElementById('amend');
+const saveButton = document.getElementById('save');
+const hiddenButton = document.getElementById('hidden');
 
-    init();
+init();
 
-    function init() {
-        change();
+function init() {
+    change();
+    hiddenButton.style.display = 'none';
+    saveButton.style.display = 'none';
+}
+
+function change() {
+    const info = select.value.split("|");
+    const id = info[0];
+    const name = info[1];
+    const address = info[2];
+    const email = info[3];
+    const website = info[4];
+    const telephone = info[5];
+
+    document.getElementById('id').value = id;
+    document.getElementById('name').value = name;
+    document.getElementById('address').value = address;
+    document.getElementById('email').value = email;
+    document.getElementById('website').value = website;
+    document.getElementById('telephone').value = telephone;
+}
+
+function toggle() {
+    if (amendButton.innerHTML === 'Amend') {
+        saveButton.style.display = 'block';
+        amendButton.innerHTML = 'View';
+
+        document.getElementById('name').disabled = false;
+        document.getElementById('address').disabled = false;
+        document.getElementById('email').disabled = false;
+        document.getElementById('website').disabled = false;
+        document.getElementById('telephone').disabled = false;
+    } else {
         saveButton.style.display = 'none';
+        amendButton.innerHTML = 'Amend';
+
+        document.getElementById('name').disabled = true;
+        document.getElementById('address').disabled = true;
+        document.getElementById('email').disabled = true;
+        document.getElementById('website').disabled = true;
+        document.getElementById('telephone').disabled = true;
     }
-
-    function change() {
-        const info = select.value.split("|");
-        const id = info[0];
-        const name = info[1];
-        const address = info[2];
-        const email = info[3];
-        const website = info[4];
-        const telephone = info[5];
-
-        document.getElementById('id').value = id;
-        document.getElementById('name').value = name;
-        document.getElementById('address').value = address;
-        document.getElementById('email').value = email;
-        document.getElementById('website').value = website;
-        document.getElementById('telephone').value = telephone;
-    }
-
-    function toggle() {
-        if (amendButton.innerHTML === 'Amend') {
-            saveButton.style.display = 'block';
-            amendButton.innerHTML = 'View';
-
-            document.getElementById('name').disabled = false;
-            document.getElementById('address').disabled = false;
-            document.getElementById('email').disabled = false;
-            document.getElementById('website').disabled = false;
-            document.getElementById('telephone').disabled = false;
-        } else {
-            saveButton.style.display = 'none';
-            amendButton.innerHTML = 'Amend';
-
-            document.getElementById('name').disabled = true;
-            document.getElementById('address').disabled = true;
-            document.getElementById('email').disabled = true;
-            document.getElementById('website').disabled = true;
-            document.getElementById('telephone').disabled = true;
-        }
-    }
+}
 
 function confirmSave() {
     if (confirm('Are you sure you want to save these changes?')) {
         document.getElementById('id').disabled = false;
         document.getElementById('select').disabled = true;
-        document.forms['form'].submit();
+        hiddenButton.click();
         document.getElementById('id').disabled = true;
         document.getElementById('select').disabled = false;
         return;

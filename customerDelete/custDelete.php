9a153bdc30fc05fbd160fddf789ece93ca53db9a
DoyleAaron <aarondoyler101@gmail.com>
2023-03-13 21:26:12 +0000

    CSS link

--- customerDelete/custDelete.php
+++ customerDelete/custDelete.php
@@ -13,7 +13,7 @@ include '../assets/php/db_connection.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pharmacy</title>
-    <link rel="stylesheet" href="../assets/css/template.css">
+    <link rel="stylesheet" href="Delete.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
 </head>

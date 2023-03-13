96746420378b1a76d77e42fe3c13b1cf8759d45d
DoyleAaron <aarondoyler101@gmail.com>
2023-03-13 21:35:22 +0000

    More Updates

--- customerDelete/delete.php
+++ customerDelete/delete.php
@@ -1,26 +1,29 @@
 <?php 
     session_start();
-    include 'menu.php';
 ?>
 <br><br>
 <?php
 
-    include 'db.inc.php';
+    include '../assets/php/db_connection.php';
 
-    $sql = "UPDATE persons SET deletedflag = true WHERE personid = '$_POST[delid]'";
+    $sql = "UPDATE Customer SET deleted = true WHERE customerID = '$_POST[delid]'";
 
     if(! mysqli_query($con, $sql)){
         echo "Error ". mysqli_error($con);
     }
 
-    $_SESSION["personid"] = $_POST['delid'];
-    $_SESSION["firstname"] = $_POST['delfirstname'];
-    $_SESSION["lastname"] = $_POST['dellastname'];
+    $_SESSION["customerID"] = $_POST['delid'];
+    $_SESSION["firstName"] = $_POST['delfirstname'];
+    $_SESSION["lastName"] = $_POST['dellastname'];
+    $_SESSION["customerAddress"] = $_POST['delcustaddress'];
+    $_SESSION["dob"] = $_POST['deldob'];
+    $_SESSION["telephoneNumber"] = $_POST['delphonenum'];
+    $_SESSION["PPSN"] = $_POST['delPPSN'];
 
     mysqli_close($con);
 
 ?>
 
 <script>
-    window.location = "delete.html.php"
+    window.location = "custDelete.php"
 </script>
